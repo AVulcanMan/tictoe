8dd54b2adb8d301d76d6840580b79097b995ce0d
YashasPatil <yashaspatil@gmail.com>
2025-03-23 20:20:29 -0700

    created html file + more logic

--- tictoe.js
+++ tictoe.js
@@ -11,10 +11,11 @@ function Gameboard () {
     }
 
     const getBoard = () => board;
-    const dropToken (player) => {
+    const dropToken (row,column,player) => {
         //Filter finds all the cells with a value of 0 and map returns them by creating a new array
         const availableCells = board.filter((row) => row[column].getValue() === 0).map(row => row[column]);
         if (!availableCells.length) return;
+        board[row][column].addToken(player);
     }
     const printBoard = () => {
         const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()))
@@ -23,3 +24,55 @@ function Gameboard () {
     
       return { getBoard, dropToken, printBoard };
 }
+function Cell() {
+    let value = 0;
+  
+    // Accept a player's token to change the value of the cell
+    const addToken = (player) => {
+      value = player;
+    };
+    const getValue = () => value;
+    return (addToken, getValue())
+}
+function Gamecontroller ( playerOneName = "Player One",
+    playerTwoName = "Player Two"
+  ) {
+    const players = [
+        {
+          name: playerOneName,
+          token: 1
+        },
+        {
+          name: playerTwoName,
+          token: 2
+        }
+      ];
+    let activePlayer = players[0]
+    function switchPlayerTurn() {
+        if (activePlayer === players[0]) {
+          activePlayer = players[1];
+        } else {
+          activePlayer = players[0];
+        }
+      }
+      const getActivePlayer = () => activePlayer;
+      const printNewRound = () => {
+        board.printBoard();
+        console.log(`${getActivePlayer().name}'s turn.`);
+      };
+      const playRound = (row, column) => {
+        // Drop a token for the current player
+        console.log(
+          `Dropping ${getActivePlayer().name}'s token into column ${row[column]}...`
+        );
+        board.dropToken(row,column, getActivePlayer().token);
+        switchPlayerTurn();
+        printNewRound();
+    }
+    printNewRound();
+    return {
+        playRound,
+        getActivePlayer
+      };
+}
+const game = GameController();
